f7158e4ebe1e644e1880e46f89715086c28a266f
Ashutosh Kumar <raj.fungus@gmail.com>
2021-01-21 20:51:23 +0530

    Added qt vars, Added Wallet Balance

--- index.js
+++ index.js
@@ -1,75 +1,40 @@
+"use strict";
 const chalk = require('chalk');
 const axios = require('axios');
-const Sentry = require('@sentry/node');
 const updateNotifier = require('update-notifier');
 
 //Update Notifications
 const pkg = require('./package.json');
-const notifier = updateNotifier({pkg});
+const notifier = updateNotifier({ pkg });
 notifier.notify();
-console.log(notifier.update);
 
-//Initialize Sentry
-Sentry.init({ dsn: 'https://8adbc6b47b784d0f8e8ef220a2455413@o386985.ingest.sentry.io/5241759' })
+let getWalletBalance = async function(authorization){
+	try{
+		const response = await axios({
+			url: 'https://www.fast2sms.com/dev/wallet',
+			params: {
+				authorization
+			}
+		})
+		return response.data
+	} catch(error){
+		return error.response.data
+	}
+}
 
 const sendMessage = async (props) => {
-	if (props.method === undefined) {
-		props.method = 'POST';
-	}
-	if (props.sender_id === undefined) {
-		props.sender_id = 'FSTSMS';
-	}
-	if (props.language === undefined) {
-		props.language = 'english';
-	}
-	if (props.route === undefined) {
-		props.route = 'p';
-	}
-	if (props.flash === undefined) {
-		props.flash = 0;
-	}
-	if (props.showLogs === undefined) {
-		props.showLogs = true;
-	}
-	//THE REASON TO GO WITH (if) and not (props.method = props.method || 'POST') is for empty String and false values.
+	const url = 'https://www.fast2sms.com/dev/bulk';
+	const {data, headers} = generateDataAndHeadersForApi(props);
+	const {method, showLogs} = props
 
-	let url = 'https://www.fast2sms.com/dev/bulk';
-	let nums = props.numbers.join(',');
-	if (props.method === 'GET') {
-		//NO-CACHE ONLY IF GET
-		data = {
-			authorization: props.authorization,
-			sender_id: props.sender_id,
-			message: props.message,
-			language: props.language,
-			route: props.route,
-			numbers: nums,
-			flash: props.flash
-		};
-		headers = {
-			'cache-control': 'no-cache'
-		};
-	} else {
-		headers = {
-			authorization: props.authorization
-		};
-
-		data = {
-			sender_id: props.sender_id,
-			message: props.message,
-			language: props.language,
-			route: props.route,
-			numbers: nums
-		};
-	}
 	try {
 		const response = await axios({
-			method: props.method,
+			method: method,
 			url,
 			data,
 			headers
 		});
-		if (props.showLogs) console.log(chalk.greenBright('Message sent successfully.'));
+		if (showLogs) console.log(chalk.greenBright('Message sent successfully.'));
 		return response.data;
 	} catch (error) {
 		if (error.response.data.status_code === 412)
@@ -82,6 +47,59 @@ const sendMessage = async (props) => {
 	}
 };
 
+
+let generateDataAndHeadersForApi = function(props) {
+	const {
+		authorization,
+		method = 'POST',
+		sender_id = 'FSTSMS',
+		language = 'english',
+		route = 'p',
+		flash = 0,
+		numbers,
+		message,
+		variables,
+		variables_values
+	} = props;
+
+	let nums = numbers.join(',');
+
+	let data = {}, headers = {};
+	
+	if (method === 'GET') {
+		data = {
+			authorization: authorization,
+			sender_id: sender_id,
+			message: message,
+			language: language,
+			route: route,
+			numbers: nums,
+			flash: flash,
+			variables,
+			variables_values
+		};
+		headers = {
+			'cache-control': 'no-cache'
+		};
+	}
+	else {
+		data = {
+			sender_id: sender_id,
+			message: message,
+			language: language,
+			route: route,
+			numbers: nums,
+			variables,
+			variables_values
+		};
+		headers = {
+			authorization: authorization
+		};
+	}
+	return {data, headers}
+}
+
 module.exports = {
-	sendMessage: sendMessage
+	sendMessage,
+	getWalletBalance
 };
